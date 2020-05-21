f9c531524c1fad94eb8ec1ff4b69eb2159c05a6b
Michael Romay <amerror@gmail.com>
2020-05-21 16:34:48 -0700

    added login function and hook

--- src/hooks/AuthProvider.js
+++ src/hooks/AuthProvider.js
@@ -2,20 +2,25 @@
 
 import React, { createContext, useState, useContext } from 'react';
 import PropTypes from 'prop-types';
-import { postSignup } from '../services/authAPI';
+import { postSignup, postLogin } from '../services/authAPI';
 
 const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
 
-  const signup = (email, password, firstName, lastName) => {
-    return postSignup(email, password, firstName, lastName)
+  const signup = (email, password, firstName, lastName, cohort, avatar) => {
+    return postSignup(email, password, firstName, lastName, cohort, avatar)
+      .then (user => setUser(user));
+  };
+
+  const login = (email, password) => {
+    return postLogin(email, password)
       .then (user => setUser(user));
   };
 
   return (
-    <AuthContext.Provider value={{ user, signup }}>
+    <AuthContext.Provider value={{ user, signup, login }}>
       {children}
     </AuthContext.Provider>
   );
@@ -28,4 +33,9 @@ AuthProvider.propTypes = {
 export const useSignUp = () =>{
   const { signup } = useContext(AuthContext);
   return signup;
+};
+
+export const useLogin = () => {
+  const { login } = useContext(AuthContext);
+  return login;
 };
