0a3267f5c5903d32fb04f1c28eadf419c7403f63
Michael Romay <amerror@gmail.com>
2020-05-22 18:06:08 -0700

    created private routes and added them to app js

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import CodeChallenges from '../CodeChallenges/CodeChallenges.jsx';
 import Challenge from '../../containers/Challenge.jsx';
 import Auth from '../Auth/Auth.jsx';
+// import PrivateRoute from '../Auth/PrivateRoute.jsx';
 export default function App() {
   return (
     <Router>
@@ -10,6 +11,8 @@ export default function App() {
         <Route exact path='/auth' component={Auth} />
         <Route exact path='/challenges' component={CodeChallenges} />
         <Route exact path='/challenges/:id' component={Challenge} />
+        {/* <PrivateRoute exact path='/challenges' component={CodeChallenges} />
+        <PrivateRoute exact path='/challenges/:id' component={Challenge} /> */}
       </Switch>
     </Router>
   );
--- src/components/CodeChallenges/CodeChallenges.jsx
+++ src/components/CodeChallenges/CodeChallenges.jsx
@@ -1,23 +1,19 @@
 import React from 'react';
-// import CodeChallenge from './CodeChallenge.jsx';
-// import { useChallenge } from '../../hooks/ChallengeProvider.jsx';
-
+import CodeChallenge from './CodeChallenge.jsx';
+import { useChallenge } from '../../hooks/ChallengeProvider.jsx';
 const CodeChallenges = () => {
-  // const codeChallenges = [];
-  // const codeElements = codeChallenges.map(codeChallenge => (
-  //   <>
-  //     <li key={codeChallenge._id}>
-  //       <CodeChallenge {...codeChallenge} />
-  //     </li>
-  //   </>
-  // ));
-
+  const codeChallenges = useChallenge();
+  const codeElements = codeChallenges.map(codeChallenge => (
+    <>
+      <li key={codeChallenge._id}>
+        <CodeChallenge {...codeChallenge} />
+      </li>
+    </>
+  ));
   return (
     <ul>
-      hello
-
+      {codeElements}
     </ul>
   );
 };
-
 export default CodeChallenges;
