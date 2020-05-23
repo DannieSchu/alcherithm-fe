5d202f04121710dfebd6a917d0a2ff913fc2f15b
Dannie Schumaker <draeschumaker@gmail.com>
2020-05-23 02:18:34 -0700

    add vector logo options for header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import styles from './Header.css';
+import logo from '../../assets/IconWhite.png';
 
 const Header = () => {
 
   return (
     <>
       <header className={styles.Header}>
-        <h1>Alcherithm</h1>
+        {/* <h1>Alcherithm</h1> */}
+        <img src={logo} />
         <nav></nav>
       </header>
     </>
