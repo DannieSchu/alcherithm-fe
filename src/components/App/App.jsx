4ed34f2b8fda0195214f018aff22449caa318f7d
Dakota Druley <58957027+dakotadruley@users.noreply.github.com>
2020-05-22 18:19:39 -0700

    Feature/code challenge page (#7)
    * removed comment
    
    * added code challenge item
    
    * added code challenge item and list and connected to provider
    
    * added the challenge display component and passed into container
    
    * updated the challenge container
    
    * updated the useEffect to set challenge . code and test
    
    * made some fixes to challenge
    
    * passed challenge state into challenge object and passed in dispaly component
    
    * add sumbit button
    
    * updated app.js

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
-// import Challenge from '../../containers/Challenge.jsx';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import CodeChallenges from '../CodeChallenges/CodeChallenges.jsx';
-
+import Challenge from '../../containers/Challenge.jsx';
+import Auth from '../Auth/Auth.jsx';
+// import PrivateRoute from '../Auth/PrivateRoute.jsx';
 export default function App() {
   return (
-    <>
-      {/* <Challenge /> */}
-      <CodeChallenges />
-    </>
+    <Router>
+      <Switch>
+        <Route exact path='/auth' component={Auth} />
+        <Route exact path='/challenges' component={CodeChallenges} />
+        <Route exact path='/challenges/:id' component={Challenge} />
+        {/* <PrivateRoute exact path='/challenges' component={CodeChallenges} />
+        <PrivateRoute exact path='/challenges/:id' component={Challenge} /> */}
+      </Switch>
+    </Router>
   );
 }
