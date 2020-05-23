fa75d15d3a17414bdf3254dc50e90515bcc8ad9f
Dakota Druley <58957027+dakotadruley@users.noreply.github.com>
2020-05-22 18:08:29 -0700

    updated app.js

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-// import Challenge from '../../containers/Challenge.jsx';
 import CodeChallenges from '../CodeChallenges/CodeChallenges.jsx';
-// import Auth from '../Auth/Auth.jsx';
 import Challenge from '../../containers/Challenge.jsx';
-
+import Auth from '../Auth/Auth.jsx';
+// import PrivateRoute from '../Auth/PrivateRoute.jsx';
 export default function App() {
   return (
     <Router>
       <Switch>
+        <Route exact path='/auth' component={Auth} />
+        <Route exact path='/challenges' component={CodeChallenges} />
         <Route exact path='/challenges/:id' component={Challenge} />
-        <Router exact path='/challenges' component={CodeChallenges} />
-        {/* <CodeChallenges /> */}
+        {/* <PrivateRoute exact path='/challenges' component={CodeChallenges} />
+        <PrivateRoute exact path='/challenges/:id' component={Challenge} /> */}
       </Switch>
     </Router>
   );
