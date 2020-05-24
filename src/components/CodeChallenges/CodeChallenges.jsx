8697d7ae2c9a5df699813b8e08a77ffe62c9142e
Dannie Schumaker <draeschumaker@gmail.com>
2020-05-23 21:13:54 -0700

    render challenges according to dropdown selection

--- src/components/CodeChallenges/CodeChallenges.jsx
+++ src/components/CodeChallenges/CodeChallenges.jsx
@@ -1,25 +1,28 @@
 import React from 'react';
 import CodeChallenge from './CodeChallenge';
+import Dropdown from '../Dropdown/Dropdown';
 import { useGetChallenges } from '../../hooks/getChallenges';
 import { Link } from 'react-router-dom';
 import styles from './CodeChallenges.css';
 
 const CodeChallenges = () => {
-  const challenges = useGetChallenges();
-  const codeElements = challenges.map(challenge => (
-    <>
-      <li key={challenge._id}>
-        <Link to={`/challenges/${challenge._id}`}>
-          <CodeChallenge {...challenge} />
-        </Link>
-      </li>
-    </>
+  const { selectedChallenges, category, setCategory } = useGetChallenges(category);
+
+  const codeElements = selectedChallenges.map(challenge => (
+    <li key={challenge._id}>
+      <Link to={`/challenges/${challenge._id}`}>
+        <CodeChallenge {...challenge} />
+      </Link>
+    </li>
   ));
 
   return (
-    <ul className={styles.CodeChallenges}>
-      {codeElements}
-    </ul>
+    <>
+      <Dropdown setCategory={setCategory} />
+      <ul className={styles.CodeChallenges}>
+        {codeElements}
+      </ul>
+    </>
   );
 };
 
