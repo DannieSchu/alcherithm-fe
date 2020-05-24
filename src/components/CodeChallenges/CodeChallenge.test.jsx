8b4474e95c8b1c571c7a99404cf7a0cd15426e7b
Dannie Schumaker <draeschumaker@gmail.com>
2020-05-24 02:17:33 -0700

    create snapshot test for CodeChallenge item component

---
+++ src/components/CodeChallenges/CodeChallenge.test.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import CodeChallenge from './CodeChallenge';
+
+describe('CodeChallenge component', () => {
+  it('renders CodeChallenge', () => {
+    const wrapper = shallow(<CodeChallenge 
+      category='forEach' 
+      challengeNumber={1} 
+      instructions ='Write a function named greeting that takes in a string and returns the string in all uppercase letters. Then, write a function named speaker that takes in a string and a callback function. The speaker function should return the string in all uppercase letters only by invoking the callback.' 
+    />);
+    expect(wrapper).toMatchSnapshot();
+  });
+});
