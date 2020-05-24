5a96f45d643bcaf68cdf60f7296df6c40d56c329
Dannie Schumaker <draeschumaker@gmail.com>
2020-05-23 21:12:38 -0700

    set selected challenges on category change

--- src/hooks/getChallenges.jsx
+++ src/hooks/getChallenges.jsx
@@ -3,10 +3,20 @@ import { fetchChallenges } from '../services/challengesAPI';
 
 export const useGetChallenges = () => {
   const [challenges, setChallenges] = useState([]);
+  const [selectedChallenges, setSelectedChallenges] = useState([]);
+  const [category, setCategory] = useState('allCategories');
 
   useEffect(() => {
     fetchChallenges()
-      .then(fetchedChallenges => setChallenges(fetchedChallenges));
+      .then(fetchedChallenges => {
+        setChallenges(fetchedChallenges);
+        setSelectedChallenges(fetchedChallenges);
+      });
   }, []);
-  return challenges;
+
+  useEffect(() => {
+    setSelectedChallenges(category === 'allCategories' ? challenges : challenges.filter(challenge => challenge.category === category));
+  }, [category]);
+
+  return { challenges, selectedChallenges, category, setCategory };
 };
