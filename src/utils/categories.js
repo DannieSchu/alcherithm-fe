4ad6b6fa3bef9801affc748b5bed5a91ba399f1b
Dannie Schumaker <draeschumaker@gmail.com>
2020-05-24 02:12:27 -0700

    update challenge categories

--- src/utils/categories.js
+++ src/utils/categories.js
@@ -1,1 +1,1 @@
-export const categories = ['ForEach', 'For Loop', 'Variables'];
+export const categories = ['forEach', 'variables', 'sort', 'string methods', 'object iteration', 'map', 'filter', 'reduce', 'chaining', 'nested loops', 'strings', 'review'];
