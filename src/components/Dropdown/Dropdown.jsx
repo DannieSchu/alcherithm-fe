7a82682ee9e92d010aaf86c5287d30f4d9a845c9
Dannie Schumaker <draeschumaker@gmail.com>
2020-05-24 02:11:57 -0700

    add a little dropdown style

--- src/components/Dropdown/Dropdown.jsx
+++ src/components/Dropdown/Dropdown.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { categories } from '../../utils/categories';
 import PropTypes from 'prop-types';
+import styles from './dropdown.css';
 
 const Dropdown = ({ setCategory }) => {
   const categoryOptions = categories.map(category => (
@@ -8,12 +9,10 @@ const Dropdown = ({ setCategory }) => {
   ));
 
   return (
-    <>
-      <select id="category" onChange={({ target }) => setCategory(target.value)}>
-        <option value="allCategories">Choose Category</option>
-        {categoryOptions}
-      </select>
-    </>
+    <select className={styles.Dropdown} id="category" onChange={({ target }) => setCategory(target.value)}>
+      <option value="allCategories">Choose Category</option>
+      {categoryOptions}
+    </select>
   );
 };
 
