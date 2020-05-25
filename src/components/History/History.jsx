c506ad12ff118f6cfca8ab9273c30e0f3db10e88
Michael Romay <amerror@gmail.com>
2020-05-25 15:15:19 -0700

    added if no user forward to splash on history

--- src/components/History/History.jsx
+++ src/components/History/History.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
-import { useLoading } from '../../hooks/AuthProvider';
+import { useLoading, useCurrentUser } from '../../hooks/AuthProvider';
+import { useHistory } from 'react-router-dom';
 
 const History = () => {
+  const history = useHistory();
+  const user = useCurrentUser();
   const loading = useLoading();
+  
   if(loading) return (
     <section>
       <h2>loading...</h2>
     </section>
   );
+
+  if(!user)
+    history.push('/splash');
 
   return (
     <h2>History goes here</h2>
