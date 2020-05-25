42d0bbd7742fcb143f53fb28e3a1f279de31be6a
Michael Romay <amerror@gmail.com>
2020-05-25 13:33:36 -0700

    added history push to login after clicking logout

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,18 +1,32 @@
 import React from 'react';
 import styles from './Header.css';
 import logo from '../../assets/IconWhite.png';
+import { Link } from 'react-router-dom';
+import { useLogout } from '../../hooks/AuthProvider';
+
+
 
 const Header = () => {
+  const logout = useLogout();
 
   return (
     <>
       <header className={styles.Header}>
         <img src={logo} />
         <h2>Alcherithm</h2>
-        <nav></nav>
+        <nav>
+          <Link to='/'>Dashboard</Link>
+          <Link to='/challenges'>Challenges</Link>
+          <Link to='/history'>History</Link>
+          <button type="button" onClick={logout}>Logout</button>
+        </nav>
       </header>
     </>
   );
 };
 
 export default Header; 
+
+
+// this.props.history.push('/login');
+{/* <button type="button" onClick={logout}>Logout</button>; */}
