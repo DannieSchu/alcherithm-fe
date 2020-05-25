e5e29a883bd5eeed2495fc1380bc136190d5991a
Michael Romay <amerror@gmail.com>
2020-05-25 16:06:17 -0700

    added links to find a challenge and history on dashboard

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { useLoading } from '../../hooks/AuthProvider';
 
 const Dashboard = () => {
@@ -11,7 +12,11 @@ const Dashboard = () => {
   );
 
   return (
-    <h2>Dashboard goes here</h2>
+    <>
+      <h2>Dashboard goes here</h2>
+      <Link to='/challenges'>Find a Challenge</Link>
+      <Link to='/history'>View Completed Challenges</Link>
+    </>
   );
 
 };
