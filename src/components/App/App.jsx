9e9a5cebc7717f5755b5986e7e8d402f7445df2f
Will G Piro <willgpiro@gmail.com>
2020-05-25 16:29:40 -0700

    add Results exact path to app.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import CodeChallenges from '../CodeChallenges/CodeChallenges.jsx';
 import Challenge from '../../containers/Challenge.jsx';
+import Results from '../../containers/Results/Results.jsx';
 import Auth from '../Auth/Auth.jsx';
 import Header from '../Header/Header';
 // import PrivateRoute from '../Auth/PrivateRoute.jsx';
@@ -14,6 +15,7 @@ export default function App() {
         <Route exact path='/auth' component={Auth} />
         <Route exact path='/challenges' component={CodeChallenges} />
         <Route exact path='/challenges/:id' component={Challenge} />
+        <Route exact path='/results' component={Results} />
         {/* <PrivateRoute exact path='/challenges' component={CodeChallenges} />
         <PrivateRoute exact path='/challenges/:id' component={Challenge} /> */}
       </Switch>
