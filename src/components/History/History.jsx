c8b3690984f2d272fd1088f6e16c0a441178ed8a
Dannie Schumaker <draeschumaker@gmail.com>
2020-05-25 23:38:52 -0700

    render user history with History component

--- src/components/History/History.jsx
+++ src/components/History/History.jsx
@@ -1,18 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
+import CodeChallenge from '../CodeChallenges/CodeChallenge';
+import Dropdown from '../Dropdown/Dropdown';
+import PopUp from '../PopUp/PopUp';
+import { useGetChallenges } from '../../hooks/getChallenges';
 import { useLoading } from '../../hooks/AuthProvider';
+import { fetchUserChallengesWithSolutions } from '../../services/solutionsAPI';
+import styles from '../CodeChallenges/CodeChallenges.css';
 
 const History = () => {
   const loading = useLoading();
+  const { selectedChallenges, setCategory } = useGetChallenges(fetchUserChallengesWithSolutions);
+  const [selectedSolutions, setSelectedSolutions] = useState([]);
+  const [visible, setVisibility] = useState(false);
 
+  const togglePopUp = () => {
+    setVisibility(!visible);
+  };
+
+  const handleClick = solutions => {
+    setSelectedSolutions(solutions.map(solution => solution.solution));
+    togglePopUp();
+  };
+
+  const codeElements = selectedChallenges.map((challenge) => (
+    <li key={challenge._id}>
+      <CodeChallenge {...challenge} />
+      <button onClick={() => handleClick(challenge.solutions)}>Your Solutions</button>
+    </li>
+  ));
+  
   if(loading) return (
     <section>
       <h2>loading...</h2>
     </section>
   );
-
+  
   return (
-    <h2>History goes here</h2>
+    <section className={styles.CodeChallenges}>
+      <Dropdown setCategory={setCategory} />
+      {visible && <PopUp toggle={togglePopUp} solutions={selectedSolutions} />}
+      <ul>
+        {codeElements}
+      </ul>
+    </section>
   );
-
 };
+
 export default History;
