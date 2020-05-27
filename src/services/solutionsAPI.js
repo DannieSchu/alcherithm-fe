1b128af4d671773840d6170f0e6602547bbee3c1
Dakota Druley <58957027+dakotadruley@users.noreply.github.com>
2020-05-27 11:23:06 -0700

    removed comments from solutionsAPI

--- src/services/solutionsAPI.js
+++ src/services/solutionsAPI.js
@@ -4,17 +4,7 @@ export const fetchUserSolutionsToChallenge = (challengeId, userId) => get(`/api/
 
 export const fetchAllSolutionsToChallenge = challengeId => get(`/api/v1/solutions?challengeId=${challengeId}`);
 
-// PREVIOUS SERVICE NAME:
-// export const fetchUserSolutions = userId => get(`/api/v1/solutions?userId=${userId}`);
-export const fetchUserChallengesWithSolutions = userId => get(`/api/v1/solutions?userId=${userId}`)
-  .then(challenges => challenges.map(challenge => ({
-    solutionId: challenge._id,
-    category: challenge.challenge.category,
-    challengeNumber: challenge.challenge.challengeNumber,
-    instructions: challenge.challenge.instructions,
-    solutions: challenge.solutions
-  })));
-
 export const fetchResults = solutionId => get(`/api/v1/solutions/results/${solutionId}`);
+
 export const fetchUserChallengesWithSolutions = userId => get(`/api/v1/solutions?userId=${userId}`);
 
