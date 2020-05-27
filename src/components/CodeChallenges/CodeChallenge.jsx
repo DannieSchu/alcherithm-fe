1a3f36facf927326dd44a7e106ec81e36749f7f4
Dannie Schumaker <draeschumaker@gmail.com>
2020-05-26 21:55:34 -0700

    render check or x and date of code challenge attempt

--- src/components/CodeChallenges/CodeChallenge.jsx
+++ src/components/CodeChallenges/CodeChallenge.jsx
@@ -2,13 +2,15 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './CodeChallenge.css';
 
-const CodeChallenge = ({ category, challengeNumber, instructions }) => {
+const CodeChallenge = ({ category, challengeNumber, instructions, solutions }) => {
   const slicedInstructions = `${instructions.slice(0, 100)}...`;
-  
+  const icon = solutions[0].passed ? '✔' : '✖️';
+
   return (
     <section className={styles.CodeChallenge}>
       <h3>{category} {challengeNumber}</h3>
       <p>{slicedInstructions}</p>
+      {solutions[0]?.date && <div><h4>{icon}</h4><h4>{solutions[0].date}</h4></div>}
     </section>
   );
 };
@@ -16,8 +18,11 @@ const CodeChallenge = ({ category, challengeNumber, instructions }) => {
 CodeChallenge.propTypes = {
   category: PropTypes.string.isRequired,
   challengeNumber: PropTypes.number.isRequired,
-  instructions: PropTypes.string.isRequired
+  instructions: PropTypes.string.isRequired,
+  solutions: PropTypes.arrayOf(PropTypes.shape({
+    passed: PropTypes.boolean,
+    date: PropTypes.string
+  }))
 };
 
 export default CodeChallenge;
-
