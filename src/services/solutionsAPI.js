ed5892011557deafdbe914911f46239e33776635
Michael Romay <amerror@gmail.com>
2020-05-27 11:22:38 -0700

    removed duplicate line that was breaking netlify

--- src/services/solutionsAPI.js
+++ src/services/solutionsAPI.js
@@ -16,5 +16,3 @@ export const fetchUserChallengesWithSolutions = userId => get(`/api/v1/solutions
   })));
 
 export const fetchResults = solutionId => get(`/api/v1/solutions/results/${solutionId}`);
-export const fetchUserChallengesWithSolutions = userId => get(`/api/v1/solutions?userId=${userId}`);
-
