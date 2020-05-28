c732ce8d77987b65f69812863392a652c63dbe23
Dannie Schumaker <draeschumaker@gmail.com>
2020-05-27 22:25:18 -0700

    update button component with color

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './buttons.css';
 import sizes from './sizes.css';
+import colors from '../../styles/colors.css';
 
-const Button = ({ onClick, buttonText, buttonStyle, buttonSize }) => <button className={`${styles[buttonStyle]} ${sizes[buttonSize]}`} onClick={onClick}>{buttonText}</button>;
+const Button = ({ onClick, buttonText, buttonStyle, buttonSize, backgroundColor }) => <button className={`${styles[buttonStyle]} ${sizes[buttonSize]} ${colors[backgroundColor]}`} onClick={onClick}>{buttonText}</button>;
 
 Button.propTypes = {
   buttonStyle: PropTypes.string,
   buttonSize: PropTypes.string,
+  backgroundColor: PropTypes.string,
   buttonText: PropTypes.string.isRequired,
   onClick: PropTypes.func
 };
