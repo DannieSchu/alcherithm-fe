2799946f565963b582cfbdd28893e969dd34bbb3
Michael Romay <amerror@gmail.com>
2020-05-28 10:39:44 -0700

    clean up userAPI

--- src/services/userAPI.js
+++ src/services/userAPI.js
@@ -1,6 +1,3 @@
 import { get } from './request';
-// import { useCurrentUser } from '../hooks/AuthProvider';
 
-// const user = useCurrentUser();
-// console.log(user);
 export const getUserPassFailAttempted = (id) => get(`/api/v1/users/${id}/counts`);
