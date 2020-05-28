bd1f76e6c9f5ee9197960c35ed685392a1d9090a
janelle mellor <janellemellor@gmail.com>
2020-05-28 12:50:10 -0700

    update progress bar imports

--- src/components/ProgressBar/ProgressBar.jsx
+++ src/components/ProgressBar/ProgressBar.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
+import { useUserAttempted, useUserTotal } from '../../hooks/AuthProvider';
 import { Progress } from 'semantic-ui-react';
 import styles from './ProgressBar.css';
 
-const ProgressBar = ({ }) => {
+const ProgressBar = () => {
+  const attempted = useUserAttempted();
+  const total = useUserTotal();
+
   return (
     <div className={styles.ProgressBar}>
-      <Progress value= total= progress='ratio' />
+      <Progress value={attempted} total={total} progress='ratio' />
     </div>
   );
 };
 
 export default ProgressBar;
-
-
