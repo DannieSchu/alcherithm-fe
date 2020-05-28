7291dbba7af74e0a4888bb68232a465faf93a45d
janelle mellor <janellemellor@gmail.com>
2020-05-28 11:33:54 -0700

    set up structure for progress bar component

---
+++ src/components/ProgressBar/ProgressBar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './ProgressBar.css';
+
+const ProgressBar = ({ }) => {
+  return (
+    <div className={styles.ProgressBar}>
+      
+    </div>
+  );
+};
+
+export default ProgressBar;
