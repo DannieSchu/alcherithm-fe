f8e60961639fad53cdbc99230bfc962bd301e833
Dakota Druley <58957027+dakotadruley@users.noreply.github.com>
2020-05-29 01:06:43 -0700

    added splash page

--- src/components/Splash/Splash.jsx
+++ src/components/Splash/Splash.jsx
@@ -5,8 +5,9 @@ import styles from './Splash.css';
 import blue1 from '../../assets/hexagons/BlueHexagon.png';
 import green from '../../assets/hexagons/GreenHexagon.png';
 import blue2 from '../../assets/hexagons/SecondaryBlueHexagon.png';
-// import combo from '../../assets/hexagons/hexagons.png';
+import combo from '../../assets/hexagons/hexagons.png';
 import logo from '../../assets/logos/CenterAlignLogo.png';
+import logo2 from '../../assets/logos/LogoCharcoal.png';
 
 const Splash = () => {
   const user = useCurrentUser();
@@ -25,20 +26,25 @@ const Splash = () => {
   return (
     <main className={styles.Splash}>
 
-      <section className={styles.container} >
-        <img src={blue1} className={styles.hexagon} />
-        <img src={blue2} className={styles.hexagon} />
-        <img src={green} className={styles.hexagon} />
+      <aside>
+        <img src={logo2} />
+      </aside>
+
+      <section className={styles.links}> 
+        <Link to='/signup'>Sign Up</Link>
       </section>
 
-      <img src={logo} />
-
-      <section>
-        <Link to='/signup'>Sign Up</Link>
+      <section className={styles.links}> 
         <Link to='/login'>Login</Link>
       </section>
+
+      <section className={styles.container} >
+        <img src={combo} className={styles.hexagon} />
+      </section>
+
     </main>
   );
 
 };
+
 export default Splash;
