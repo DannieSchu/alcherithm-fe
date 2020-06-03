a3565dd7638f5bf39f7cc9337a58cbec5e547b34
Dannie Schumaker <draeschumaker@gmail.com>
2020-06-02 22:54:27 -0700

    replace buttonText prop with children

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -4,18 +4,18 @@ import styles from './buttons.css';
 import sizes from './sizes.css';
 import colors from '../../styles/colors.css';
 
-const Button = ({ 
-  onClick, 
-  buttonText, 
+const Button = ({  
+  children, 
   buttonStyle, 
   buttonSize, 
-  backgroundColor }) => <button className={`${styles[buttonStyle]} ${sizes[buttonSize]} ${colors[backgroundColor]}`} onClick={onClick}>{buttonText}</button>;
+  backgroundColor,
+  onClick }) => <button className={`${styles[buttonStyle]} ${sizes[buttonSize]} ${colors[backgroundColor]}`} onClick={onClick}>{children}</button>;
 
 Button.propTypes = {
   buttonStyle: PropTypes.string,
   buttonSize: PropTypes.string,
   backgroundColor: PropTypes.string,
-  buttonText: PropTypes.string.isRequired,
+  children: PropTypes.string.isRequired,
   onClick: PropTypes.func
 };
 
