005c9f0eae2eeceed0d611ff517fcb06113533bc
Dannie Schumaker <draeschumaker@gmail.com>
2020-06-03 00:06:27 -0700

    remove unnecessary conditionals from Dashboard

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -6,7 +6,7 @@ import DashboardHeader from './DashboardHeader';
 import DashboardAnalytics from './DashboardAnalytics';
 
 import { useCurrentUser, useUserAttempted } from '../../hooks/AuthProvider';
-import styles from './Dashboard.css'; 
+import styles from './Dashboard.css';
 
 const Dashboard = () => {
   const user = useCurrentUser();
@@ -14,79 +14,12 @@ const Dashboard = () => {
 
   if(!user) return <Loading />;
 
-  // if(user.cohort === 'N/A' && attempted === 0) return (
-  //   <main className={styles.DashboardAttemptedZero}>
-  //     {/* <section className={styles.userProgress}>
-  //       <section className={styles.textContainer}>
-  //         <h2>{user.firstName}'s Dashboard!</h2>
-  //       </section>    
-  //     </section> */}
-  //     <h2 className={styles.trySome}>try some challenges to see your stats!</h2>
-  //     <section className={styles.buttons}>
-  //       <Link to="/challenges">
-  //         <Button buttonStyle="primary" backgroundColor="green" buttonSize="large">Find a Challenge</Button>
-  //       </Link>
-  //       <Link to="/history">
-  //         <Button buttonStyle="primary" backgroundColor="mainBlue" buttonSize="large">View History</Button>
-  //       </Link>
-  //     </section>
-  //   </main>
-  // );
-
-  // if(user.cohort === 'N/A') return (
-  //   <main className={styles.Dashboard}>
-  //     {/* <section className={styles.userProgress}>
-  //       <section className={styles.textContainer}>
-  //         <h2>{user.firstName}'s Dashboard!</h2>
-  //       </section>
-  //       <ProgressBar />
-  //     </section> */}
-  //     <section className={styles.charts}>
-  //       <SuccessChart />
-  //       <PassedByCategoryChart />
-  //     </section>
-  //     <section className={styles.buttons}>
-  //     <Link to="/challenges">
-  //         <Button buttonStyle="primary" backgroundColor="green" buttonSize="large">Find a Challenge</Button>
-  //       </Link>
-  //       <Link to="/history">
-  //         <Button buttonStyle="primary" backgroundColor="mainBlue" buttonSize="large">View History</Button>
-  //       </Link>
-  //     </section>
-  //   </main>
-  // );
-
-  // if(attempted === 0) return (
-  //   <main className={styles.DashboardAttemptedZero}>
-  //     {/* <section className={styles.userProgress}>
-  //       <section className={styles.textContainer}>
-  //         <h2>{user.firstName}'s Dashboard!</h2>
-  //         {user.Cohort && <h3>Cohort: {user.cohort}</h3>}
-  //       </section>    
-  //     </section> */}
-  //     <h2 className={styles.trySome}>try some challenges to see your stats!</h2>
-  //     <section className={styles.buttons}>
-  //       <Link to="/challenges">
-  //         <Button buttonStyle="primary" backgroundColor="green" buttonSize="large">Find a Challenge</Button>
-  //       </Link>
-  //       <Link to="/history">
-  //         <Button buttonStyle="primary" backgroundColor="mainBlue" buttonSize="large">View History</Button>
-  //       </Link>
-  //     </section>
-  //   </main>
-  // );
-
   return (
     <main className={styles.Dashboard}>
-      {/* <section className={styles.userProgress}>
-        <section className={styles.textContainer}>
-          <h2>{user.firstName}'s Dashboard!</h2>
-          {user.Cohort && <h3>Cohort: {user.cohort}</h3>}
-        </section>
-        <ProgressBar />
-      </section> */}
-      <DashboardHeader {...user} attempted={attempted} />
-      <DashboardAnalytics />
+      <section className={styles.box}>
+        <DashboardHeader {...user} attempted={attempted} />
+        {attempted ? <DashboardAnalytics /> : <h2>Try some challenges to see your stats!</h2>}
+      </section>
       <section className={styles.buttons}>
         <Link to="/challenges">
           <Button buttonStyle="primary" backgroundColor="green" buttonSize="large">Find a Challenge</Button>
