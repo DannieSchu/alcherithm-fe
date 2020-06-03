0e93e65ab2e127f4d1a12660700fed88322d3e36
Dannie Schumaker <draeschumaker@gmail.com>
2020-06-02 21:36:22 -0700

    move developers array to utility file

---
+++ src/utils/developers.js
@@ -0,0 +1,43 @@
+import Dakota from '../assets/profile/Dakota.png';
+import Dannie from '../assets/profile/Dannie.jpg';
+import Janelle from '../assets/profile/Janelle.png';
+import Mikey from '../assets/profile/Mikey.png';
+import Will from '../assets/profile/Will.jpg';
+
+export const developers = [
+  {
+    name: 'Dakota',
+    image: Dakota,
+    gitHub: 'https://github.com/dakotadruley',
+    linkedIn: 'https://www.linkedin.com/in/dakota-druley/',
+    description: 'DAKOTA DRULEY enjoys the full stack process and is passionate about problem solving, integrations, and efficient code. She is also a dancer, painter, social justice advocate and proud cat mom.'
+  },
+  {
+    name: 'Dannie',
+    image: Dannie,
+    gitHub: 'https://github.com/DannieSchu',
+    linkedIn: 'https://www.linkedin.com/in/dannie-schumaker/',
+    description: 'DANNIE SCHUMAKER is a lifelong creator with a passion for the intersection of science and art. Whether knitting a sweater or building an app, her favorite part of the creative process is finding an elegant solution to a complex problem.'
+  },
+  {
+    name: 'Janelle',
+    image: Janelle,
+    gitHub: 'https://github.com/janellemellor',
+    linkedIn: 'https://www.linkedin.com/in/janellemellor/',
+    description: 'JANELLE MELLOR is a fullstack developer who enjoys leveraging her technical experience with her community building background to develop meaningful tools. She uses her lived/learned experience and privilege to support the creation of more human-centered teams, processes, and programs and finds grounding in nature and in connection with others.'
+  },
+  {
+    name: 'Mikey',
+    image: Mikey,
+    gitHub: 'https://github.com/mikeymasonic',
+    linkedIn: 'https://www.linkedin.com/in/michaelromay/',
+    description: 'MICHAEL ROMAY is a fullstack engineer, installation artist and musician who makes weird digital visual and interactive art.  Code, music and art go hand in hand for him.  He also has a baby bird named Rico that is very sweet.'
+  },
+  {
+    name: 'Will',
+    image: Will,
+    gitHub: 'https://github.com/Shaka2Pass',
+    linkedIn: 'https://www.linkedin.com/in/willpiro/',
+    description: 'WILL PIRO is full stack software engineer who relishes in the mentality that every project deserves a clear goal, detailed plan, sensible testing framework, and the grit to execute. As a former City Planner, he looks forward to building applications that exist at the intersection of sustainable community development and web-based technologies that produce positive, tangible, and equitable outcomes.'
+  }
+];
