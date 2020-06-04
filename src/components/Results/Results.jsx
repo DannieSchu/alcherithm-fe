2521f3054d4d547668a27bc3ba2a52ccb89fd448
Dannie Schumaker <draeschumaker@gmail.com>
2020-06-04 02:19:22 -0700

    create component for congratulatory panda

--- src/components/Results/Results.jsx
+++ src/components/Results/Results.jsx
@@ -1,9 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import ConfettiCanvas from 'react-confetti-canvas';
-import GifLoader from 'react-gif-loader';
 import Button from '../Button/Button';
 import Tabs from '../Tabs/Tabs';
+import PandaGreeting from './PandaGreeting';
 import SolutionsViewer from './SolutionsViewer';
 import { useCurrentUser } from '../../hooks/AuthProvider';
 import { useGetSolutions } from '../../hooks/getSolutions';
@@ -11,7 +11,7 @@ import styles from './Results.css';
 
 const Results = () => {
   const { userSolutions, sampleSolution } = useGetSolutions();
-  const user = useCurrentUser();
+  const { firstName } = useCurrentUser();
   const [confetti, setConfetti] = useState(true);
   const [selectedTab, setSelectedTab] = useState('Your Solutions');
 
@@ -28,14 +28,6 @@ const Results = () => {
     };
   }, []);
 
-  const style = {
-    marginTop: '8%',
-    position: 'relative',
-    right: '50%',
-    width: '15rem',
-    'z-index': '0'
-  };
-
   return (
     <main className={styles.Results}>
       {confetti && <ConfettiCanvas className={styles.confetti}
@@ -47,16 +39,7 @@ const Results = () => {
           ['#30E89F', '#76D7C4'],
           ['#F0B27A', '#7DCEA0']]} />}
       <section className={styles.left}>
-        <section className={styles.displayName}>
-          <h3>Nice Work, {user.firstName}!</h3>
-          <div className={styles.panda}>
-            <GifLoader
-              loading={true}
-              imageSrc="https://emojis.slackmojis.com/emojis/images/1498192826/2489/dancingpanda.gif?1498192826"
-              imageStyle={style}
-              overlayBackground="rgba(0,0,0,0)" />
-          </div>
-        </section>
+        <PandaGreeting firstName={firstName} />
         <aside className={styles.buttons}>
           <Link to="/challenges">
             <Button buttonStyle="primary" buttonSize="small" backgroundColor="green">New Challenge</Button>
@@ -68,13 +51,13 @@ const Results = () => {
       </section>
 
       <section className={styles.right}>
-        <Tabs 
-          tabNames={['Your Solutions', 'Sample Solution']} 
-          selectedTab={selectedTab} 
+        <Tabs
+          tabNames={['Your Solutions', 'Sample Solution']}
+          selectedTab={selectedTab}
           onChange={handleTabChange} />
         {selectedTab === 'Your Solutions' && <SolutionsViewer solutions={userSolutions} heading={selectedTab} />}
-        {selectedTab === 'Sample Solution' && 
-        <SolutionsViewer solutions={[sampleSolution]} heading={selectedTab} />}
+        {selectedTab === 'Sample Solution' &&
+          <SolutionsViewer solutions={[sampleSolution]} heading={selectedTab} />}
       </section>
     </main>
   );
