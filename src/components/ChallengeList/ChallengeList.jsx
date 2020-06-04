c72d9b6b782f047da256bafd20f3f066d69e5ae3
Dannie Schumaker <draeschumaker@gmail.com>
2020-06-03 22:52:13 -0700

    rename CodeChallenges component ChallengeList

--- src/components/ChallengeList/ChallengeList.jsx
+++ src/components/ChallengeList/ChallengeList.jsx
@@ -1,32 +1,32 @@
 import React from 'react';
-import CodeChallenge from './CodeChallenge';
+import ChallengeItem from '../ChallengeItem/ChallengeItem';
 import Dropdown from '../Dropdown/Dropdown';
 import { useGetChallenges } from '../../hooks/getChallenges';
 import { fetchChallenges } from '../../services/challengesAPI';
 import { useGetCombinedLoading } from '../../hooks/getLoading';
 import { Link } from 'react-router-dom';
-import styles from './CodeChallenges.css';
+import styles from './ChallengeList.css';
 
-const CodeChallenges = () => {
+const ChallengeList = () => {
   const { selectedChallenges, category, setCategory, loading } = useGetChallenges(fetchChallenges);
 
   const codeElements = selectedChallenges.map(challenge => (
     <li key={challenge._id}>
       <Link to={`/challenges/${challenge._id}`}>
-        <CodeChallenge {...challenge} char={100} />
+        <ChallengeItem {...challenge} char={100} />
       </Link>
     </li>
   ));
 
   return (
-    <section className={styles.CodeChallenges}>
+    <section className={styles.ChallengeList}>
       <Dropdown setCategory={setCategory} category={category} />
       {useGetCombinedLoading(loading)}
-      <ul className={styles.row}>
+      <ul>
         {codeElements}
       </ul>
     </section>
   );
 };
 
-export default CodeChallenges;
+export default ChallengeList;
